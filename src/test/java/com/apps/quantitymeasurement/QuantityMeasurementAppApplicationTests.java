a382d94250eb8b066f8c9caac068727f0264e615
utkarshmishra88 <utkarshmishra8871@gmail.com>
2026-03-21 18:29:25 +0530

    [Utkarsh] : Added initial setup for UC17

---
+++ src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apps.quantitymeasurement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantityMeasurementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
